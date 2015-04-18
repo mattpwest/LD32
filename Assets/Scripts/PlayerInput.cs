a5392c91f3ba20094641689fb58e86cbf98ef6fe
sas.vanderwest <sas.vanderwest@entelect.co.za>
2015-04-18 11:18:30 +0200

    Created initial horizontal movement

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+	
+	public float maxSpeed = 10f;
+	private bool facingRight = true;
+	private Rigidbody2D body;
+	
+	float move;
+	
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		move = Input.GetAxis ("Horizontal");
+		
+		body.velocity = new Vector2(move * maxSpeed, body.velocity.y);
+	}
+}
