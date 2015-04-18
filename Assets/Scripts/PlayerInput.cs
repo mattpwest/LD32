e3ef5f0d4a2fe23701bc46bfd4d053926d9eaf96
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2015-04-18 22:19:21 +0200

    Player movement is finally kinda decent...

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 using AssemblyCSharp;
+using System;
 
 public class PlayerInput : MonoBehaviour {
-	
-	public float maxSpeed = 10f;
+
 	public float jumpForce = 250f;
+	public float maxSpeed = 3.0f;
 	private FacingDirection facing = FacingDirection.Right;
 	private Rigidbody2D body;
 	public Animator animator;
 
 	public bool grounded = false;
 	public bool shouldJump = false;
+	public bool shooting = false;
+	public float shootStrength = 0.0f;
+	public float shootChargeRate = 1.0f / 60.0f;
 	public Transform groundCheck;
 	float groundRadius = 0.1f;
 	public LayerMask whatIsGround;
@@ -21,6 +25,7 @@ public class PlayerInput : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		body = GetComponent<Rigidbody2D> ();
+		//body.drag = 0.4f;
 	}
 	
 	// Update is called once per frame
@@ -30,38 +35,50 @@ public class PlayerInput : MonoBehaviour {
 		HandleInput ();
 
 		animator.SetBool ("Grounded", grounded);
-		animator.SetBool ("Walking", body.velocity.x != 0);
+		animator.SetBool ("Walking", Math.Abs (body.velocity.x) > 0.1f);
 
 		UpdateFacing ();
 	}
 
-	void FixedUpdate () {
-		HandleInputPhysics ();
-	}
-
 	void HandleInput(){
 		move = Input.GetAxis (Inputs.Horizontal);
-		
+
 		shouldJump = Input.GetButtonDown (Inputs.Jump) && grounded;
+		
+		if ((!shooting) && (Input.GetButtonDown (Inputs.Shoot))) {
+			shootStrength += shootChargeRate;
+		} else if (shootStrength > 0.0f) {
+			Shoot();
+		}
 	}
 
-	void HandleInputPhysics(){
-		body.velocity = new Vector2(move * maxSpeed, body.velocity.y);
-
+	void FixedUpdate () {
+		body.velocity = new Vector2 (move * maxSpeed, body.velocity.y);
+		
 		if (shouldJump) {
-			body.AddForce(new Vector2(0, jumpForce));
+			body.AddForce (new Vector2 (0, jumpForce));
 		}
 	}
 
 	void UpdateFacing(){
-		var newFacing = body.velocity.x > 0 ? FacingDirection.Right : FacingDirection.Left;
-		if ((body.velocity.x == 0) || (newFacing.Equals (facing))) {
-			return;
+		var newFacing = facing;
+		if (body.velocity.x > 0.1f) {
+			newFacing = FacingDirection.Right;
+		} else if (body.velocity.x < -0.1f) {
+			newFacing = FacingDirection.Left;
 		}
 
 		facing = newFacing;
 		var theScale = body.transform.localScale;
 		theScale.x = (int) facing;
 		body.transform.localScale = theScale;
+	}
+
+	void Shoot() {
+		shooting = true;
+
+		// TODO: Spawn bullet
+
+		shootStrength = 0.0f;
 	}
 }
