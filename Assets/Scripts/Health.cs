70131f1d789593422b44b39336521545e6825a00
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2015-04-19 15:00:38 +0200

    Added Health component which destroys the entity when it reaches 0 health. Added GUI to display player Health. Projectiles now damage health.

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public int HP = 2;
+	public int HPInitial = 2;
+
+	void Start () {
+		HP = HPInitial;
+	}
+	
+	void Update () {
+		if (HP <= 0) {
+			Destroy (gameObject);
+		}
+	}
+
+	public void Damage(int damage) {
+		HP -= damage;
+	}
+}
---
+++ Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour {
+
+	public int halfValue = 1;
+	public int fullValue = 2;
+	private Health health;
+	private Image image;
+	public Sprite emptySprite;
+	public Sprite halfSprite;
+	public Sprite fullSprite;
+
+	void Start () {
+		GameObject player = GameObject.Find("Player1");
+		health = player.GetComponent<Health>();
+		image = GetComponentInParent<Image>();
+	}
+	
+	void Update () {
+		if (health.HP >= fullValue) {
+			this.image.sprite = fullSprite;
+		} else if (health.HP == halfValue) {
+			this.image.sprite = halfSprite;
+		} else {
+			this.image.sprite = emptySprite;
+		}
+	}
+}
