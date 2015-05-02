b20971e0fb58e40015b052415216c4b1ea8fc0da
sas.vanderwest <sas.vanderwest@entelect.co.za>
2015-05-02 13:23:16 +0200

    Added methods to interface

---
+++ Assets/Scripts/Input/CustInput.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using AssemblyCSharp;
+
+namespace AssemblyCSharp
+{
+	public interface Input {
+		float GetAxis(InputAxis axis);
+		bool GetButtonDown(Inputs button);
+		bool GetButtonUp(Inputs button);
+	}
+}
